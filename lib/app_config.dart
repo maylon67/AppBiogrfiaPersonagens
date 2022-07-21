d4f2ec1eb534dc3f29d1ccd4caa1574d140eef26
Maylon <maylongatodemais25@gmail.com>
2022-07-21 07:55:45 -0300

    terceiro commit

---
+++ lib/app_config.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+
+
+double width(BuildContext context) => MediaQuery.of(context).size.width;
+double height(BuildContext context) => MediaQuery.of(context).size.height;
--- lib/main_page.dart
+++ lib/main_page.dart
@@ -1,6 +1,7 @@
 import 'package:app_personagens_biografia/models/cards.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:app_personagens_biografia/app_config.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
@@ -8,9 +9,8 @@ class MainPage extends StatefulWidget {
   @override
   State<MainPage> createState() => _MainPageState();
 }
-class _MainPageState extends State<MainPage> {
-  final List<Cards> listaCards = [];
 
+class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,11 +19,48 @@ class _MainPageState extends State<MainPage> {
           'Biografia de Personagens',
           style: GoogleFonts.permanentMarker(
               color: Color(0xFFe39c0e), fontSize: 26),
-              textAlign: TextAlign.center,
+          textAlign: TextAlign.center,
         ),
         backgroundColor: Color.fromARGB(255, 19, 18, 18),
       ),
-      
+      body:SizedBox(
+        height:height(context)*0.8,
+        width: width(context),
+        child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 15, vertical: 25),
+        child: GridView(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              mainAxisSpacing: 10,
+              crossAxisSpacing: 7.5),
+          shrinkWrap: true,
+          children: [
+            Container(
+              color: Colors.blue,
+            ),
+            Container(color: Colors.red),
+            Container(
+              color: Colors.yellow,
+            ),
+            Container(
+              color: Colors.grey,
+            ),
+            Container(
+              color: Colors.green,
+            ),
+            Container(
+              color: Colors.pink,
+            ),
+            Container(color: Colors.blue),
+            Container(color: Colors.red),
+            Container(
+              color: Colors.yellow,
+            ),
+          ],
+        ),
+      ),
+      )
+       
     );
   }
 }
